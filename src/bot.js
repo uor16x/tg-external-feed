4f74c7c314aa179963a4e0cd2270742cd50fb6d2
uor16x <u0r.home@gmail.com>
2022-04-12 17:31:12 +0300

    TODO plan changes

--- src/bot.js
+++ src/bot.js
@@ -62,8 +62,7 @@ const utils = {
 const _methods = {
     start: db => vk => bot => async msg => {
         const id = msg.chat.id
-        db.getOrCreateUser(id)
-        await (require('./queue')).run()
+        // TODO: hello message with help
         await bot.sendMessage(
             id,
             `Welcome, ${msg.chat.first_name}!`,
@@ -73,52 +72,54 @@ const _methods = {
     sources: db => vk => bot => async msg => {
         const id = msg.chat.id
 
-        const sources = db.getSourcesByUserId(id)
-        const answerText = utils.getSourcesText(sources, msg.chat.first_name)
-        const answerKeyboard = utils.getSourcesReplyMarkup(sources)
-        await bot.sendMessage(
-            id,
-            answerText,
-            { 
-                reply_markup: answerKeyboard,
-                reply_to_message_id: msg.message_id
-            }
-        )
+        // TODO: sources list as keyboard
+        // const sources = db.getSourcesByUserId(id)
+        // const answerText = utils.getSourcesText(sources, msg.chat.first_name)
+        // const answerKeyboard = utils.getSourcesReplyMarkup(sources)
+        // await bot.sendMessage(
+        //     id,
+        //     answerText,
+        //     { 
+        //         reply_markup: answerKeyboard,
+        //         reply_to_message_id: msg.message_id
+        //     }
+        // )
     },
     update: db => vk => bot => async msg => {
-        const id = msg.chat.id
+        // TODO: remove this method
+        // const id = msg.chat.id
 
-        const feedMsg = await bot.sendMessage(
-            id,
-            'Preparing your feed...'
-        )
-        const sources = db.getSourcesByUserId(id)
-        const post = await groups.formFeed(id, sources)
-        const feedText = utils.formatPost(post)
+        // const feedMsg = await bot.sendMessage(
+        //     id,
+        //     'Preparing your feed...'
+        // )
+        // const sources = db.getSourcesByUserId(id)
+        // const post = await groups.formFeed(id, sources)
+        // const feedText = utils.formatPost(post)
 
-        await bot.sendMediaGroup(
-            id,
-            [{
-                caption: feedText,
-                type: 'photo',
-                media: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png'
-            }, {
-                type: 'photo',
-                media: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png'
-            }, {
-                type: 'photo',
-                media: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png'
-            }]
-        )
-        await bot.sendMessage(
-            id,
-            '➤➤➤',
-            {
-                reply_markup: keyboard
-                    .next
-                    .getMarkup({ resize_keyboard: true })
-            }
-        )
+        // await bot.sendMediaGroup(
+        //     id,
+        //     [{
+        //         caption: feedText,
+        //         type: 'photo',
+        //         media: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png'
+        //     }, {
+        //         type: 'photo',
+        //         media: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png'
+        //     }, {
+        //         type: 'photo',
+        //         media: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png'
+        //     }]
+        // )
+        // await bot.sendMessage(
+        //     id,
+        //     '➤➤➤',
+        //     {
+        //         reply_markup: keyboard
+        //             .next
+        //             .getMarkup({ resize_keyboard: true })
+        //     }
+        // )
 
         // TODO: refactor to separated method
         // if (post.atts) {
@@ -149,62 +150,61 @@ const _methods = {
         // })
     },
     addSource: db => vk => bot => async msg => {
-        const id = msg.chat.id
-        const url = msg.text
-        const msg_id = msg.message_id
+        // TODO: add user to source's array
+        // const id = msg.chat.id
+        // const url = msg.text
+        // const msg_id = msg.message_id
 
-        const progressMsg = await bot.sendMessage(
-            id,
-            `Processing...`,
-            { reply_to_message_id: msg_id }
-        )
-        let groupData = null
-        try {
-            groupData = await vk.getGroupData(url)
-        } catch (groupDataReceiveErr) {
-            return bot.editMessageText(`Failed to process the group: ${groupDataReceiveErr.message}`, {
-                chat_id: id,
-                message_id: progressMsg.message_id
-            })
-        }
-        try {
-            db.addSource(id, groupData)
-        } catch (saveSourceErr) {
-            return bot.editMessageText(`Failed to save the source: ${saveSourceErr.message}`, {
-                chat_id: id,
-                message_id: progressMsg.message_id
-            })
-        }
-        bot.editMessageText(`Processing done.\n${groupData.name} saved.`, {
-            chat_id: id,
-            message_id: progressMsg.message_id
-        })
-    },
-    q: db => vk => bot => async msg => {
-        await bot.deleteMessage(msg.chat.id, msg.message_id)
+        // const progressMsg = await bot.sendMessage(
+        //     id,
+        //     `Processing...`,
+        //     { reply_to_message_id: msg_id }
+        // )
+        // let groupData = null
+        // try {
+        //     groupData = await vk.getGroupData(url)
+        // } catch (groupDataReceiveErr) {
+        //     return bot.editMessageText(`Failed to process the group: ${groupDataReceiveErr.message}`, {
+        //         chat_id: id,
+        //         message_id: progressMsg.message_id
+        //     })
+        // }
+        // try {
+        //     db.addSource(id, groupData)
+        // } catch (saveSourceErr) {
+        //     return bot.editMessageText(`Failed to save the source: ${saveSourceErr.message}`, {
+        //         chat_id: id,
+        //         message_id: progressMsg.message_id
+        //     })
+        // }
+        // bot.editMessageText(`Processing done.\n${groupData.name} saved.`, {
+        //     chat_id: id,
+        //     message_id: progressMsg.message_id
+        // })
     },
     deleteSource: db => vk => bot => async ({ data, query }) => {
-        const id = query.from.id
-        try {
-            const name = db.deleteSource(id, data)
-            const sources = db.getSourcesByUserId(id)
-            const replyMarkup = utils.getSourcesReplyMarkup(sources)
-            if (!replyMarkup) {
-                const text = utils.getSourcesText(sources, query.message.chat.first_name)
-                return bot.editMessageText(text, {
-                    chat_id: id,
-                    message_id: query.message.message_id
-                })
-            }
-            await bot.editMessageReplyMarkup(replyMarkup, {
-                chat_id: id,
-                message_id: query.message.message_id
-            })
-            await bot.sendMessage(id, `${name} deleted.`)
-        } catch (err) {
-            console.error(`Failed to delete the group: ${err}`)
-            await bot.sendMessage(id, `Failed to delete the group.`)
-        }
+        // TODO: remove user from sources arr
+        // const id = query.from.id
+        // try {
+        //     const name = db.deleteSource(id, data)
+        //     const sources = db.getSourcesByUserId(id)
+        //     const replyMarkup = utils.getSourcesReplyMarkup(sources)
+        //     if (!replyMarkup) {
+        //         const text = utils.getSourcesText(sources, query.message.chat.first_name)
+        //         return bot.editMessageText(text, {
+        //             chat_id: id,
+        //             message_id: query.message.message_id
+        //         })
+        //     }
+        //     await bot.editMessageReplyMarkup(replyMarkup, {
+        //         chat_id: id,
+        //         message_id: query.message.message_id
+        //     })
+        //     await bot.sendMessage(id, `${name} deleted.`)
+        // } catch (err) {
+            // console.error(`Failed to delete the group: ${err}`)
+            // await bot.sendMessage(id, `Failed to delete the group.`)
+        // }
     }
 }
 
@@ -234,7 +234,6 @@ function configureBot(bot, methods) {
     bot.onText(/Sources/, methods.sources)
     bot.onText(/Update/, methods.update)
     bot.onText(vkRegex, methods.addSource)
-    bot.onText(/\/next/, methods.q)
 
 
     bot.on('callback_query', async query => {
